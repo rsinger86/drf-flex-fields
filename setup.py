59edb54fa832f48171df20c4627e89a19351db6d
robertgsinger <robert.singer@theabisgroup.com>
2016-12-26 22:19:49 -0600

    renamed module

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='drf-dynamic-serializer',
+setup(name='drf-flex-fields',
       version='0.1.0',
-      description='Dynamic serialization, including fields and nested models, for Django REST Framework',
+      description='Flexible, dynamic fields and nested models for Django REST Framework serializers.',
       author='Robert Singer',
       author_email='robertgsinger@gmail.com',
-      url='https://www.python.org/sigs/distutils-sig/',
       packages=['dynamic_serializer'],
      )
