d84e7c591f3059fed58e0dc1abe9659955c4ad4b
robertgsinger <robert.singer@theabisgroup.com>
2016-12-28 20:23:22 -0600

    added django test app to simplify testing

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,8 +1,19 @@
 import unittest
+from rest_flex_fields import FlexFieldsModelSerializer
+from rest_framework import serializers
+from testapp.models import Pet
 
 
 class TestFlexFieldSerializer(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(1,1)
+        class PetSerializer(FlexFieldsModelSerializer):
+            class Meta:
+                model = Pet  
+                fields = ['name', 'toys', 'species']
 
+        Serializer = PetSerializer
+        pet = Pet(name='Remy')
+        serializer = Serializer(pet)
+        # print(serializer.data)
+        self.assertEqual(1,1)
---
+++ tests/testapp/models.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    public = models.CharField(max_length=30)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    hobbies = models.CharField(max_length=30)
+    employer = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=30)
+    toys = models.CharField(max_length=30)
+    species = models.CharField(max_length=30)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+
