8bf8ab90c831de4eadb54fa0a0880d4507d9e774
Chris Bailey <chris.bailey@onyxinsight.com>
2018-09-29 17:21:29 +0100

    Corrected comment

--- rest_flex_fields/serializers.py
+++ rest_flex_fields/serializers.py
@@ -8,7 +8,7 @@ from rest_flex_fields import split_levels
 class FlexFieldsSerializerMixin(object):
     """
         A ModelSerializer that takes additional arguments for
-        "fields" and "include" in order to
+        "fields" and "omit" in order to
         control which fields are displayed, and whether to replace simple values with
         complex, nested serializations.
     """
