55c850df78526413a45286bfa3b12fcf40804dac
Chris Bailey <chris.bailey@onyxinsight.com>
2018-10-04 10:42:51 +0100

    Provide viewsets the facility to perform select/prefetch related type optimisations when expanding fields

--- rest_flex_fields/views.py
+++ rest_flex_fields/views.py
@@ -1,40 +1,56 @@
 """ 
-	This class helps provide control over which fields can be expanded when a 
-	collection is request via the list method.
+    This class helps provide control over which fields can be expanded when a
+    collection is requested via the list method.
 """
-
 from rest_framework import viewsets
 
 
 class FlexFieldsMixin(object):
-	permit_list_expands = []
-	_expandable = True
-	_force_expand = []
-	    
-	def list(self, request, *args, **kwargs):
-		"""
-			Prevent expansion by default; add fields to "permit_list_expands"
-			to whitelist particular fields.
-		"""
-		self._expandable = False
-		expand = request.query_params.get('expand')
+    permit_list_expands = []
+    _expandable = True
+    _force_expand = []
 
-		if len(self.permit_list_expands) > 0 and expand:
-			if expand == '*':
-				self._force_expand = self.permit_list_expands
-			else:
-				self._force_expand = list( 
-					set(expand.split(',')) & set(self.permit_list_expands) 
-				)
+    def list(self, request, *args, **kwargs):
+        """
+            Prevent expansion by default; add fields to "permit_list_expands"
+            to whitelist particular fields.
+        """
+        self._expandable = False
+        expand = request.query_params.get('expand')
 
-		return super(FlexFieldsMixin, self).list(request, *args, **kwargs)
-	
+        if len(self.permit_list_expands) > 0 and expand:
+            if expand == '*':
+                self._force_expand = self.permit_list_expands
+            else:
+                self._force_expand = list(
+                    set(expand.split(',')) & set(self.permit_list_expands)
+                )
 
-	def get_serializer_context(self):
-		default_context = super(FlexFieldsMixin, self).get_serializer_context()
-		default_context['expandable'] = self._expandable
-		default_context['force_expand'] = self._force_expand
-		return default_context
+        return super(FlexFieldsMixin, self).list(request, *args, **kwargs)
+
+    def get_serializer_context(self):
+        default_context = super(FlexFieldsMixin, self).get_serializer_context()
+        default_context['expandable'] = self._expandable
+        default_context['force_expand'] = self._force_expand
+        return default_context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        expand = self.request.query_params.get('expand')
+
+        if not expand:
+            return queryset
+
+        expand_fields = self.get_serializer_class().expandable_fields.keys() if expand == '*' else set(expand.split(','))
+        valid_expand_fields = expand_fields if self._expandable else set(expand_fields) & set(self._force_expand)
+
+        for field in valid_expand_fields:
+            queryset = self.expand_field(field, queryset)
+
+        return queryset
+
+    def expand_field(self, field, queryset):
+        return queryset
 
 
 class FlexFieldsModelViewSet(FlexFieldsMixin, viewsets.ModelViewSet):
