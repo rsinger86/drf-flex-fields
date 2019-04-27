725ffdc9b0a76cc28d55e0c492c21bf485397f6d
Robert Singer <robertgsinger@gmail.com>
2019-04-27 10:54:12 -0500

    Adds more test cases

--- rest_flex_fields/serializers.py
+++ rest_flex_fields/serializers.py
@@ -13,15 +13,16 @@ class FlexFieldsSerializerMixin(object):
         control which fields are displayed, and whether to replace simple
         values with complex, nested serializations.clean
     """
+
     expandable_fields = {}
 
     def __init__(self, *args, **kwargs):
         self.expanded_fields = []
 
         passed = {
-            'expand': kwargs.pop('expand', []),
-            'fields': kwargs.pop('fields', []),
-            'omit': kwargs.pop('omit', [])
+            "expand": kwargs.pop("expand", []),
+            "fields": kwargs.pop("fields", []),
+            "omit": kwargs.pop("omit", []),
         }
 
         super(FlexFieldsSerializerMixin, self).__init__(*args, **kwargs)
@@ -33,28 +34,22 @@ class FlexFieldsSerializerMixin(object):
         sparse_fields, next_sparse_fields = split_levels(fields)
         omit_fields, next_omit_fields = split_levels(omit)
 
-        self._clean_fields(
-            omit_fields, sparse_fields, next_omit_fields
-        )
+        self._clean_fields(omit_fields, sparse_fields, next_omit_fields)
 
         expanded_field_names = self._get_expanded_names(
-            expand_fields,
-            omit_fields,
-            sparse_fields,
-            next_omit_fields
+            expand_fields, omit_fields, sparse_fields, next_omit_fields
         )
 
         for name in expanded_field_names:
             self.expanded_fields.append(name)
 
             self.fields[name] = self._make_expanded_field_serializer(
-                name,
-                next_expand_fields,
-                next_sparse_fields,
-                next_omit_fields
+                name, next_expand_fields, next_sparse_fields, next_omit_fields
             )
 
-    def _make_expanded_field_serializer(self, name, nested_expand, nested_fields, nested_omit):
+    def _make_expanded_field_serializer(
+        self, name, nested_expand, nested_fields, nested_omit
+    ):
         """
         Returns an instance of the dynamically created nested serializer.
         """
@@ -63,16 +58,16 @@ class FlexFieldsSerializerMixin(object):
         serializer_settings = copy.deepcopy(field_options[1])
 
         if name in nested_expand:
-            serializer_settings['expand'] = nested_expand[name]
+            serializer_settings["expand"] = nested_expand[name]
 
         if name in nested_fields:
-            serializer_settings['fields'] = nested_fields[name]
+            serializer_settings["fields"] = nested_fields[name]
 
         if name in nested_omit:
-            serializer_settings['omit'] = nested_omit[name]
+            serializer_settings["omit"] = nested_omit[name]
 
-        if serializer_settings.get('source') == name:
-            del serializer_settings['source']
+        if serializer_settings.get("source") == name:
+            del serializer_settings["source"]
 
         if type(serializer_class) == str:
             serializer_class = self._import_serializer_class(serializer_class)
@@ -85,13 +80,13 @@ class FlexFieldsSerializerMixin(object):
         <app>.<SerializerName> will automatically be interpreted as:
         <app>.serializers.<SerializerName>
         """
-        pieces = location.split('.')
+        pieces = location.split(".")
         class_name = pieces.pop()
 
-        if pieces[len(pieces)-1] != 'serializers':
-            pieces.append('serializers')
+        if pieces[len(pieces) - 1] != "serializers":
+            pieces.append("serializers")
 
-        module = importlib.import_module('.'.join(pieces))
+        module = importlib.import_module(".".join(pieces))
         return getattr(module, class_name)
 
     def _clean_fields(self, omit_fields, sparse_fields, next_level_omits):
@@ -116,11 +111,9 @@ class FlexFieldsSerializerMixin(object):
         for remove_field in to_remove:
             self.fields.pop(remove_field)
 
-    def _should_field_exist(self,
-                            field_name,
-                            omit_fields,
-                            sparse_fields,
-                            next_level_omits):
+    def _should_field_exist(
+        self, field_name, omit_fields, sparse_fields, next_level_omits
+    ):
         """
             Next level omits take form of:
             {
@@ -137,15 +130,13 @@ class FlexFieldsSerializerMixin(object):
 
         return True
 
-    def _get_expanded_names(self,
-                            expand_fields,
-                            omit_fields,
-                            sparse_fields,
-                            next_level_omits):
+    def _get_expanded_names(
+        self, expand_fields, omit_fields, sparse_fields, next_level_omits
+    ):
         if len(expand_fields) == 0:
             return []
 
-        if '~all' or '*' in expand_fields:
+        if "~all" in expand_fields or "*" in expand_fields:
             expand_fields = self.expandable_fields.keys()
 
         accum = []
@@ -176,42 +167,42 @@ class FlexFieldsSerializerMixin(object):
         if self.parent:
             return False
 
-        if not hasattr(self, 'context') or not self.context.get('request', None):
+        if not hasattr(self, "context") or not self.context.get("request", None):
             return False
 
-        return self.context['request'].method == 'GET'
+        return self.context["request"].method == "GET"
 
     def _get_omit_input(self, passed_settings):
-        value = passed_settings.get('omit')
+        value = passed_settings.get("omit")
 
         if len(value) > 0:
             return value
 
-        if not self._can_access_request:
-            return []
-
-        omit = self.context['request'].query_params.get('omit')
-        return omit.split(',') if omit else []
+        return self._parse_request_list_value("omit")
 
     def _get_fields_input(self, passed_settings):
-        value = passed_settings.get('fields')
+        value = passed_settings.get("fields")
 
         if len(value) > 0:
             return value
 
+        return self._parse_request_list_value("fields")
+
+    def _parse_request_list_value(self, field):
         if not self._can_access_request:
             return []
 
-        fields = self.context['request'].query_params.get('fields')
-        return fields.split(',') if fields else []
+        value = self.context["request"].query_params.get(field)
+        return value.split(",") if value else []
 
     def _get_expand_input(self, passed_settings):
         """
-            If not expandable (ViewSet list method set this to false),
-            check to see if there are any fields that we are forcing
-            to be expanded (from permit_list_expands).
+            If expand value is explicitliy passed, just return it.
+            If parsing from request, ensure that the value complies with
+            the "permitted_expands" list passed into the context from the
+            FlexFieldsMixin.
         """
-        value = passed_settings.get('expand')
+        value = passed_settings.get("expand")
 
         if len(value) > 0:
             return value
@@ -219,17 +210,18 @@ class FlexFieldsSerializerMixin(object):
         if not self._can_access_request:
             return []
 
-        if self.context.get('expandable') is False:
-            force_expand = self.context.get('force_expand', [])
-            if len(force_expand) > 0:
-                return force_expand
+        expand = self._parse_request_list_value("expand")
 
-            return []
+        if "permitted_expands" in self.context:
+            permitted_expands = self.context["permitted_expands"]
 
-        expand = self.context['request'].query_params.get('expand')
-        return expand.split(',') if expand else []
+            if "~all" in expand or "*" in expand:
+                return permitted_expands
+            else:
+                return list(set(expand) & set(permitted_expands))
+
+        return expand
 
 
-class FlexFieldsModelSerializer(FlexFieldsSerializerMixin,
-                                serializers.ModelSerializer):
+class FlexFieldsModelSerializer(FlexFieldsSerializerMixin, serializers.ModelSerializer):
     pass
--- tests/test_flex_fields_model_serializer.py
+++ tests/test_flex_fields_model_serializer.py
@@ -6,44 +6,85 @@ from rest_flex_fields import FlexFieldsModelSerializer
 
 
 class MockRequest(object):
-
-    def __init__(self, query_params={}, method='GET'):
+    def __init__(self, query_params={}, method="GET"):
         self.query_params = query_params
         self.method = method
 
 
 class TestFlexFieldModelSerializer(TestCase):
-
     def test_field_should_not_exist_if_omitted(self):
         serializer = FlexFieldsModelSerializer()
-        result = serializer._should_field_exist('name', ['name'], [], {})
+        result = serializer._should_field_exist("name", ["name"], [], {})
         self.assertFalse(result)
 
     def test_field_should_not_exist_if_not_in_sparse(self):
         serializer = FlexFieldsModelSerializer()
-        result = serializer._should_field_exist('name', [], ['age'], {})
+        result = serializer._should_field_exist("name", [], ["age"], {})
         self.assertFalse(result)
 
     def test_field_should_exist_if_ommitted_but_is_parent_of_omit(self):
         serializer = FlexFieldsModelSerializer()
 
         result = serializer._should_field_exist(
-            'employer', ['employer'], [], {'employer': ['address']}
+            "employer", ["employer"], [], {"employer": ["address"]}
         )
 
         self.assertTrue(result)
 
     def test_clean_fields(self):
         serializer = FlexFieldsModelSerializer()
-        serializer._fields = {'cat': 1, 'dog': 2, 'zebra': 3}
-        serializer._clean_fields(['cat'], [], {})
-        self.assertEqual(serializer.fields, {'dog': 2, 'zebra': 3})
+        serializer._fields = {"cat": 1, "dog": 2, "zebra": 3}
+        serializer._clean_fields(["cat"], [], {})
+        self.assertEqual(serializer.fields, {"dog": 2, "zebra": 3})
 
-    def test_get_expanded_names(self):
-        return
+    def test_get_expanded_names_if_all(self):
+        serializer = FlexFieldsModelSerializer()
+        serializer.expandable_fields = {"cat": "field", "dog": "field"}
+        result = serializer._get_expanded_names("*", [], [], {})
+        self.assertEqual(result, ["cat", "dog"])
 
-    def test_can_access_request(self):
-        return
+    def test_get_expanded_names_but_not_omitted(self):
+        serializer = FlexFieldsModelSerializer()
+        serializer.expandable_fields = {"cat": "field", "dog": "field"}
+        result = serializer._get_expanded_names(["cat", "dog"], ["cat"], [], {})
+        self.assertEqual(result, ["dog"])
+
+    def test_get_expanded_names_but_only_sparse(self):
+        serializer = FlexFieldsModelSerializer()
+        serializer.expandable_fields = {"cat": "field", "dog": "field"}
+        result = serializer._get_expanded_names(["cat"], [], ["cat"], {})
+        self.assertEqual(result, ["cat"])
+
+    def test_get_expanded_names_even_omitted_when_defer_to_next_level(self):
+        serializer = FlexFieldsModelSerializer()
+        serializer.expandable_fields = {"cat": "field", "dog": "field"}
+        result = serializer._get_expanded_names(["cat"], ["cat"], [], {"cat": ["age"]})
+        self.assertEqual(result, ["cat"])
+
+    def test_cannot_access_request_if_not_root_serializer(self):
+        serializer = FlexFieldsModelSerializer()
+        serializer.parent = "Another serializer here"
+        self.assertFalse(serializer._can_access_request)
+
+    def test_cannot_access_request_if_no_context(self):
+        serializer = FlexFieldsModelSerializer()
+        self.assertFalse(serializer._can_access_request)
+
+    def test_cannot_access_request_if_context_missing_request(self):
+        serializer = FlexFieldsModelSerializer(context={})
+        self.assertFalse(serializer._can_access_request)
+
+    def test_cannot_access_request_if_request_method_is_not_get(self):
+        serializer = FlexFieldsModelSerializer(
+            context={"request": MockRequest(method="PUT")}
+        )
+        self.assertFalse(serializer._can_access_request)
+
+    def test_can_access_request_if_request_method_is_get(self):
+        serializer = FlexFieldsModelSerializer(
+            context={"request": MockRequest(method="GET")}
+        )
+        self.assertTrue(serializer._can_access_request)
 
     def test_get_omit_input(self):
         return
