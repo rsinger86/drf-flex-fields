adeb797fd3232533fbcd5cea1e09568b11dd1163
Robert Singer <robertgsinger@gmail.com>
2020-02-08 15:21:13 -0600

    Updates requirements for test app

--- tests/urls.py
+++ tests/urls.py
@@ -1,14 +1,10 @@
-
 from django.conf.urls import url, include
 from rest_framework import routers
 from tests.testapp.views import PetViewSet
 
 
-
 # Standard viewsets
 router = routers.DefaultRouter()
-router.register(r'pets', PetViewSet, base_name='pet')
+router.register(r"pets", PetViewSet, basename="pet")
 
-urlpatterns = [
-    url(r'^', include(router.urls))
-]
+urlpatterns = [url(r"^", include(router.urls))]
